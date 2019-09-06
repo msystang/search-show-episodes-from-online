0cfc5ee3f85dc633e071d5dda2f53f89c872db5e
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-06 15:07:50 -0400

    adds code for NetworkManager and AppError, creates ShowAPIClient file

--- search-show-episodes-from-online/search-show-episodes-from-online/Clients/NetworkManager.swift
+++ search-show-episodes-from-online/search-show-episodes-from-online/Clients/NetworkManager.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    private init() {}
+    
+    static let shared = NetworkManager()
+    
+    func getData(from urlStr: String, completionHandler: @escaping (Result<Data, AppError>) -> () ) {
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            DispatchQueue.main.async {
+                
+                guard let data = data else {
+                    completionHandler(.failure(.noDataReceived))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    completionHandler(.failure(.badStatusCode))
+                    return
+                }
+                
+                if let error = error {
+                    let error = error as NSError
+                    if error.domain == NSURLErrorDomain && error.code == NSURLErrorNotConnectedToInternet {
+                        completionHandler(.failure(.noInternetConnection))
+                        return
+                    } else {
+                        completionHandler(.failure(.other(rawError: error)))
+                        return
+                    }
+                }
+                completionHandler(.success(data))
+            }
+        } .resume()
+    }
+    
+}
