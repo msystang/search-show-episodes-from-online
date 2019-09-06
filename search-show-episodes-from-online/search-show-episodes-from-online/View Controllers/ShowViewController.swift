c4169dca1897b2677459cf5213251d8afe9c012e
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-06 17:27:19 -0400

    adds extension for tableview and searchbar delegates in ShowVC

--- search-show-episodes-from-online/search-show-episodes-from-online/View Controllers/ShowViewController.swift
+++ search-show-episodes-from-online/search-show-episodes-from-online/View Controllers/ShowViewController.swift
@@ -11,11 +11,43 @@ import UIKit
 class ShowViewController: UIViewController {
 
     // URL to load shows: "http://api.tvmaze.com/search/shows?q=\(searchResultHere)"
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var showTableView: UITableView!
+    
+    var shows: [Show]? {
+        didSet {
+            showTableView.reloadData()
+        }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+        configureSearchBar()
+    }
+    
+    private func configureTableView() {
+        showTableView.delegate = self
+        showTableView.dataSource = self
+    }
+    
+    private func configureSearchBar() {
+        searchBar.delegate = self
+    }
 
 }
 
+extension ShowViewController: UITableViewDelegate {}
+
+extension ShowViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ShowViewController: UISearchBarDelegate {}
