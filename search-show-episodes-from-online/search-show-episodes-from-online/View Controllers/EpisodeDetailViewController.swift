c9a574c1b16275fd508ca59002a405d1fb410b5b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-10 17:32:11 -0400

    adds outlets to EpisodeDetailVC

--- search-show-episodes-from-online/search-show-episodes-from-online/View Controllers/EpisodeDetailViewController.swift
+++ search-show-episodes-from-online/search-show-episodes-from-online/View Controllers/EpisodeDetailViewController.swift
@@ -9,25 +9,19 @@
 import UIKit
 
 class EpisodeDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var episodeImage: UIImageView!
+    @IBOutlet weak var episodeNameLabel: UILabel!
+    @IBOutlet weak var seasonEpNumLabel: UILabel!
+    @IBOutlet weak var epDescriptionTextView: UITextView!
+    
+    
     var episode: Episode!
     // URL to load episode: "http://api.tvmaze.com/shows/\(episideIDhere)/episodes?=summary"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
